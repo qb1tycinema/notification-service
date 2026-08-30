de2799eeb68210739b28d316a0924d3f14ceddd9
qb1ty <arsenbro0707@mail.ru>
2026-08-30 20:46:00 +0500

    chore: configured the rmq service

---
+++ src/infrastructure/rmq/rmq.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { RmqContext } from '@nestjs/microservices';
+
+@Injectable()
+export class RmqService {
+    private readonly logger = new Logger(RmqService.name)
+
+    public ack(context: RmqContext): void {
+        const channel = context.getChannelRef()
+        const message = context.getMessage()
+        const tag = message?.field?.deliveryTag
+
+        if (!tag) return
+
+        channel.ack(message)
+
+        this.logger.debug(`ACK (pattern: ${context.getPattern()}, tag: ${tag})`)
+    }
+
+    public nack(context: RmqContext, requeue = false): void {
+        const channel = context.getChannelRef()
+        const message = context.getMessage()
+        const tag = message?.field?.deliveryTag
+
+        if (!tag) return
+
+        channel.nack(
+            message,
+            false,
+            requeue
+        )
+
+        if (requeue) {
+            this.logger.warn(
+                `NACK response (pattern: ${context.getPattern()}, tag: ${tag})`
+            )
+        } else {
+            this.logger.error(
+                `NACK drop (pattern: ${context.getPattern()}, tag: ${tag})`
+            )
+        }
+    }
+}
