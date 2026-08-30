baa640fb33d059d6ed590495ad4f9529cd980fbf
qb1ty <arsenbro0707@mail.ru>
2026-08-30 20:30:30 +0500

    feat: microservice connection via the rmq transport is configured

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core"
+
+import { AppModule } from "./app.module"
+import { Transport, type MicroserviceOptions } from "@nestjs/microservices"
+import { ConfigService } from "@nestjs/config"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(AppModule)
+
+	const config = app.get(ConfigService)
+
+	app.connectMicroservice<MicroserviceOptions>({
+		transport: Transport.RMQ,
+		options: {
+			urls: [config.getOrThrow<string>("RMQ_URL")],
+			queue: config.getOrThrow<string>("RMQ_QUEUE"),
+			queueOptions: {
+				durable: true
+			},
+			noAck: false,
+			prefetchCount: 1,
+			persistent: true
+		}
+	})
+
+	await app.startAllMicroservices()
+	await app.init()
 }
-bootstrap();
+bootstrap()
