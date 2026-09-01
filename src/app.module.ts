49ff748186f2852fdf6c352cfeb765de623fdb03
qb1ty <arsenbro0707@mail.ru>
2026-09-02 00:45:30 +0500

    feat(core): setup global ConfigModule with zod validation loader

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from "@nestjs/common"
 import { RmqModule } from './infrastructure/rmq/rmq.module';
 import { ConfigModule } from "@nestjs/config";
+import { NotificationsModule } from './modules/notifications/notifications.module';
+import { MailModule } from './infrastructure/mail/mail.module';
+import { configuration } from "./config";
 
 @Module({
 	imports: [
-		ConfigModule.forRoot({ isGlobal: true }),
-		RmqModule
+		ConfigModule.forRoot({
+			isGlobal: true,
+			load: [configuration],
+			expandVariables: true
+		}),
+		RmqModule,
+		NotificationsModule,
+		MailModule
 	],
 	controllers: [],
 	providers: []
