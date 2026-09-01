6ba5e0f24670d03b6efdd5984d7579fdac2303f5
qb1ty <arsenbro0707@mail.ru>
2026-09-02 01:00:29 +0500

    feat(config): add smtp variables to zod validation schema with type coercion

--- src/config/validation.schema.ts
+++ src/config/validation.schema.ts
@@ -8,6 +8,14 @@ export enum Environemt {
 
 export default z.object({
 	NODE_ENV: z.enum(Environemt).default(Environemt.Development),
+
 	RMQ_URL: z.string().nonempty(),
-	RMQ_QUEUE: z.string().nonempty()
+	RMQ_QUEUE: z.string().nonempty(),
+
+	SMTP_HOST: z.string().nonempty(),
+	SMTP_PORT: z.coerce.number(),
+	SMTP_USERNAME: z.string().nonempty(),
+	SMTP_PASSWORD: z.string().nonempty(),
+	SMTP_FROM_ADDRESS: z.string().email().nonempty(),
+	SMTP_SECURE: z.string().transform(v => v === "true")
 })
