455482f5f511dd186be202978c51fcb54f372b7d
qb1ty <arsenbro0707@mail.ru>
2026-09-02 00:44:50 +0500

    refactor(core): use get instead of getOrThrow for rmq config retrieval

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,8 @@ async function bootstrap() {
 	app.connectMicroservice<MicroserviceOptions>({
 		transport: Transport.RMQ,
 		options: {
-			urls: [config.getOrThrow<string>("RMQ_URL")],
-			queue: config.getOrThrow<string>("RMQ_QUEUE"),
+			urls: config.get("rmq.url"),
+			queue: config.get("rmq.queue"),
 			queueOptions: {
 				durable: true
 			},
