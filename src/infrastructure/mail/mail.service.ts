683e5cbb5cb70af4d26b9095a436971378060d10
qb1ty <arsenbro0707@mail.ru>
2026-09-03 23:15:53 +0500

    feat(mail): integrate TemplateService for dynamic otp email rendering

--- src/infrastructure/mail/mail.service.ts
+++ src/infrastructure/mail/mail.service.ts
@@ -1,19 +1,22 @@
-import { MailerService } from "@nestjs-modules/mailer";
+import { MailerService } from "@nestjs-modules/mailer"
 import { Injectable } from "@nestjs/common"
+
+import { TemplateService } from "./template.service"
 
 @Injectable()
 export class MailService {
-    public constructor(
-        private readonly mailerService: MailerService
-    ) {}
+	public constructor(
+		private readonly mailerService: MailerService,
+		private readonly templateService: TemplateService
+	) {}
 
-    public async sendOtp(email: string, code: string) {
-        const html = `<h1>Ваш код ${code}</h1>`
+	public async sendOtp(email: string, code: string) {
+		const html = await this.templateService.render("otp", { code })
 
-        await this.mailerService.sendMail({
-            to: email,
-            subject: "Ваш код подтверждения",
-            html
-        })
-    }
+		await this.mailerService.sendMail({
+			to: email,
+			subject: "Ваш код подтверждения",
+			html
+		})
+	}
 }
