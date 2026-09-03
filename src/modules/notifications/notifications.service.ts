3b6811671cbc569c7abdc29d1f5868bf49db2bf3
qb1ty <arsenbro0707@mail.ru>
2026-09-03 23:14:46 +0500

    fix: format file fix

--- src/modules/notifications/notifications.service.ts
+++ src/modules/notifications/notifications.service.ts
@@ -1,22 +1,21 @@
-import { MailService } from "@/infrastructure/mail/mail.service";
 import { Injectable } from "@nestjs/common"
-import type { OtpRequestedEvent } from "@qb1tycinema/contracts";
+import type { OtpRequestedEvent } from "@qb1tycinema/contracts"
+
+import { MailService } from "@/infrastructure/mail/mail.service"
 
 @Injectable()
 export class NotificationsService {
-    public constructor(
-        private readonly mailService: MailService
-    ) {}
+	public constructor(private readonly mailService: MailService) {}
 
-    public async sendOtp(data: OtpRequestedEvent) {
-        const { identifier, code, type } = data
-        
-        if (type === "email") {
-            await this.mailService.sendOtp(identifier, code)
-        }
+	public async sendOtp(data: OtpRequestedEvent) {
+		const { identifier, code, type } = data
 
-        if (type === "phone") {
-            console.log("SMS")
-        }
-    }
+		if (type === "email") {
+			await this.mailService.sendOtp(identifier, code)
+		}
+
+		if (type === "phone") {
+			console.log("SMS")
+		}
+	}
 }
