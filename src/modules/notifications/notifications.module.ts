c54ffb120032a958c96c2bf599e37047e88d0862
qb1ty <arsenbro0707@mail.ru>
2026-09-04 01:05:38 +0500

    feat(notifications): wire MailModule and dynamic SmsModule into NotificationsModule

--- src/modules/notifications/notifications.module.ts
+++ src/modules/notifications/notifications.module.ts
@@ -1,11 +1,20 @@
 import { Module } from "@nestjs/common"
+import { ConfigService } from "@nestjs/config"
 
 import { NotificationsController } from "./notifications.controller"
 import { NotificationsService } from "./notifications.service"
+import { getSmsConfig } from "@/config/factories"
 import { MailModule } from "@/infrastructure/mail/mail.module"
+import { SmsModule } from "@/infrastructure/sms/sms.module"
 
 @Module({
-	imports: [MailModule],
+	imports: [
+		MailModule,
+		SmsModule.registerAsync({
+			useFactory: getSmsConfig,
+			inject: [ConfigService]
+		})
+	],
 	controllers: [NotificationsController],
 	providers: [NotificationsService]
 })
