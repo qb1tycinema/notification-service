113522ddf047b289bc5e275632efde7495344c47
qb1ty <arsenbro0707@mail.ru>
2026-09-21 23:51:07 +0500

    feat(observability): register RMQ metrics and global interceptor in MetricsModule

---
+++ src/observability/metrics/metrics.module.ts
@@ -0,0 +1,57 @@
+import { Global, Module } from "@nestjs/common"
+import {
+	makeCounterProvider,
+	makeHistogramProvider,
+	PrometheusModule
+} from "@willsoto/nestjs-prometheus"
+
+import { RmqModule } from "@/infrastructure/rmq/rmq.module"
+import { RmqMetricsInterceptor } from "./rmq-metrics.interceptors"
+import { APP_INTERCEPTOR } from "@nestjs/core"
+
+@Global()
+@Module({
+	imports: [
+		PrometheusModule.register({
+			path: "/metrics",
+			defaultMetrics: {
+				enabled: true
+			}
+		}),
+		RmqModule
+	],
+	providers: [
+		makeHistogramProvider({
+			name: "rmq_event_processing_duration_seconds",
+			help: "RabbitMQ event processing duration",
+			labelNames: ["service", "event"],
+			buckets: [0.05, 0.1, 0.2, 0.5, 1, 2, 5]
+		}),
+		makeCounterProvider({
+			name: "rmq_events_total",
+			help: "Total RabbitMQ events processing",
+			labelNames: ["service", "event", "status"]
+		}),
+		makeCounterProvider({
+			name: "rmq_events_ack_total",
+			help: "Total ACKed RabbitMQ events",
+			labelNames: ["service", "event"]
+		}),
+		makeCounterProvider({
+			name: "rmq_events_nack_total",
+			help: "Total NACKed RabbitMQ events",
+			labelNames: ["service", "event"]
+		}),
+		{
+			provide: APP_INTERCEPTOR,
+			useClass: RmqMetricsInterceptor
+		}
+	],
+	exports: [
+		"PROM_METRIC_RMQ_EVENT_PROCESSING_DURATION_SECONDS",
+		"PROM_METRIC_RMQ_EVENTS_TOTAL",
+		"PROM_METRIC_RMQ_EVENTS_ACK_TOTAL",
+		"PROM_METRIC_RMQ_EVENTS_NACK_TOTAL"
+	]
+})
+export class MetricsModule {}
