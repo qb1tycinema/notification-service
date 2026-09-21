54445db46afd506c972105083ceab6508e8cd16c
qb1ty <arsenbro0707@mail.ru>
2026-09-21 23:52:35 +0500

    refactor(notifications): remove boilerplate from controller in favor of global metrics interceptor

--- src/modules/notifications/notifications.controller.ts
+++ src/modules/notifications/notifications.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Logger } from "@nestjs/common"
-import { Ctx, EventPattern, Payload, RmqContext } from "@nestjs/microservices"
+import { Controller } from "@nestjs/common"
+import { EventPattern, Payload } from "@nestjs/microservices"
 import type {
 	EmailChangeEvent,
 	OtpRequestedEvent,
@@ -7,62 +7,25 @@ import type {
 } from "@qb1tycinema/contracts"
 
 import { NotificationsService } from "./notifications.service"
-import { RmqService } from "@/infrastructure/rmq/rmq.service"
 
 @Controller()
 export class NotificationsController {
-	private readonly logger = new Logger(NotificationsController.name)
-
 	public constructor(
 		private readonly notificationsService: NotificationsService,
-		private readonly rmqService: RmqService
 	) {}
 
 	@EventPattern("auth.otp.requested")
-	public async otpRequested(
-		@Payload() data: OtpRequestedEvent,
-		@Ctx() ctx: RmqContext
-	) {
-		try {
-			await this.notificationsService.sendOtp(data)
-
-			this.rmqService.ack(ctx)
-		} catch (error) {
-			this.logger.error("OTP proccessing error: ", error)
-
-			this.rmqService.nack(ctx)
-		}
+	public async otpRequested(@Payload() data: OtpRequestedEvent) {
+		await this.notificationsService.sendOtp(data)
 	}
 
 	@EventPattern("account.phone.change")
-	public async phoneChange(
-		@Payload() data: PhoneChangeEvent,
-		@Ctx() ctx: RmqContext
-	) {
-		try {
-			await this.notificationsService.phoneChange(data)
-
-			this.rmqService.ack(ctx)
-		} catch (error) {
-			this.logger.error("Phone change proccessing error: ", error)
-
-			this.rmqService.nack(ctx)
-		}
+	public async phoneChange(@Payload() data: PhoneChangeEvent) {
+		await this.notificationsService.phoneChange(data)
 	}
 
 	@EventPattern("account.email.change")
-	public async emailChange(
-		@Payload() data: EmailChangeEvent,
-		@Ctx() ctx: RmqContext
-	) {
-		try {
-			await this.notificationsService.emailChange(data)
-
-			this.rmqService.ack(ctx)
-		} catch (error) {
-			this.logger.error("Email change proccessing error: ", error)
-
-			this.rmqService.nack(ctx)
-		}
+	public async emailChange(@Payload() data: EmailChangeEvent) {
+		await this.notificationsService.emailChange(data)
 	}
 }
