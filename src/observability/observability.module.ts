b8a09dc8378a4caf3f3196403b17638933bc4d0f
qb1ty <arsenbro0707@mail.ru>
2026-09-22 20:13:36 +0500

    feat(observability): register RmqMetricsInterceptor and RmqModule at ObservabilityModule level

--- src/observability/observability.module.ts
+++ src/observability/observability.module.ts
@@ -1,8 +1,17 @@
 import { Module } from "@nestjs/common"
+import { APP_INTERCEPTOR } from "@nestjs/core"
 
 import { MetricsModule } from "./metrics/metrics.module"
+import { RmqMetricsInterceptor } from "./metrics/rmq-metrics.interceptors"
+import { RmqModule } from "@/infrastructure/rmq/rmq.module"
 
 @Module({
-	imports: [MetricsModule]
+	imports: [MetricsModule, RmqModule],
+	providers: [
+		{
+			provide: APP_INTERCEPTOR,
+			useClass: RmqMetricsInterceptor
+		}
+	]
 })
 export class ObservabilityModule {}
