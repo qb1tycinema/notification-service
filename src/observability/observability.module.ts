8ef61e825fb2bce9a118d837d8af260d71d05023
qb1ty <arsenbro0707@mail.ru>
2026-09-22 20:15:54 +0500

    refactor(observability): remove global interceptor to prevent double execution

--- src/observability/observability.module.ts
+++ src/observability/observability.module.ts
@@ -1,17 +1,9 @@
 import { Module } from "@nestjs/common"
-import { APP_INTERCEPTOR } from "@nestjs/core"
 
 import { MetricsModule } from "./metrics/metrics.module"
-import { RmqMetricsInterceptor } from "./metrics/rmq-metrics.interceptors"
 import { RmqModule } from "@/infrastructure/rmq/rmq.module"
 
 @Module({
-	imports: [MetricsModule, RmqModule],
-	providers: [
-		{
-			provide: APP_INTERCEPTOR,
-			useClass: RmqMetricsInterceptor
-		}
-	]
+	imports: [MetricsModule, RmqModule]
 })
 export class ObservabilityModule {}
